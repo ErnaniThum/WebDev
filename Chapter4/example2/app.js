a2ea3b540c6c5c0890d0bb4574ab0d3e51a86893
Ernani Vinicius Thum <Vinicius@Ernanis-MacBook-Pro.local>
2016-05-05 23:24:54 -0300

    added jQuery basic funcions

--- Chapter4/example2/app.js
+++ Chapter4/example2/app.js
@@ -4,9 +4,13 @@ var main = function () {
 		var $new_comment = $("<p>");
 		var $input;
 		$input = $(".comment-input input").val();
-		$new_comment.text($input);
-		$(".comments").append($new_comment);
-
+		if ($input != ""){
+			$new_comment.text($input);
+			$(".comments").append($new_comment);
+			$(".comment-input input").val("");
+			$(".inputAlert").attr('id', '');
+		}
+		else $(".inputAlert").attr('id', 'showAlert'); 
 	});
 };
 
