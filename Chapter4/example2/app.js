0dfc2cb814bba08ab0c24d3c75de0313c1583057
Ernani Vinicius Thum <Vinicius@Ernanis-MacBook-Pro.local>
2016-05-09 16:59:20 -0300

    melhorias no exemplo 4

--- Chapter4/example2/app.js
+++ Chapter4/example2/app.js
@@ -1,16 +1,36 @@
 var main = function () {
 	"use strict";
+	
 	$(".comment-input button").on("click", function (event) {
 		var $new_comment = $("<p>");
 		var $input;
+
 		$input = $(".comment-input input").val();
 		if ($input != ""){
 			$new_comment.text($input);
-			$(".comments").append($new_comment);
+			$new_comment.hide();$(".comments").append($new_comment);
 			$(".comment-input input").val("");
 			$(".inputAlert").attr('id', '');
 		}
 		else $(".inputAlert").attr('id', 'showAlert'); 
+	});
+
+	$(".comment-input input").on("keypress", function(event){
+		// console.log("Button code " + event.keyCode + " pressed.");
+		var $new_comment = $("<p>");
+		var $input;
+		
+		if (event.keyCode == 13){
+			$input = $(".comment-input input").val();
+			if ($input != ""){
+				$new_comment.text($input);
+				$(".comments").append($new_comment);
+				$(".comment-input input").val("");
+				$(".inputAlert").attr('id', '');
+			}
+			else $(".inputAlert").attr('id', 'showAlert'); 
+
+		}
 	});
 };
 
