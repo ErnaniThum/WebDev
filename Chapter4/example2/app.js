c82f33f2f6a049add26c3c8830dcd62f106ee5af
Ernani Vinicius Thum <viniciusthm@hotmail.com>
2016-05-09 19:08:20 -0300

    added remove button on example4

--- Chapter4/example2/app.js
+++ Chapter4/example2/app.js
@@ -1,38 +1,83 @@
+
 var main = function () {
 	"use strict";
 	
-	$(".comment-input button").on("click", function (event) {
-		var $new_comment = $("<p>");
-		var $input;
-
-		$input = $(".comment-input input").val();
-		if ($input != ""){
-			$new_comment.text($input);
-			$new_comment.hide();$(".comments").append($new_comment);
+	var addCommentFromInputBox = function () {
+		var $new_comment;
+		if ($(".comment-input input").val() !== "") {
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
 			$(".comment-input input").val("");
-			$(".inputAlert").attr('id', '');
+
+			$(".inputAlert").hide();
 		}
-		else $(".inputAlert").attr('id', 'showAlert'); 
+		else $(".inputAlert").fadeIn();
+	};
+
+	function removeCommentFromInputBox() {
+		
+		var lastParagraph = $(".comments p").size();
+		if (lastParagraph > 0){
+			$(".comments p")[lastParagraph-1].remove();
+		}
+	};
+
+	// Trigger click message field
+	$("button.add").on("click", function (event) {
+		addCommentFromInputBox();
 	});
 
-	$(".comment-input input").on("keypress", function(event){
-		// console.log("Button code " + event.keyCode + " pressed.");
-		var $new_comment = $("<p>");
-		var $input;
-		
-		if (event.keyCode == 13){
-			$input = $(".comment-input input").val();
-			if ($input != ""){
-				$new_comment.text($input);
-				$(".comments").append($new_comment);
-				$(".comment-input input").val("");
-				$(".inputAlert").attr('id', '');
-			}
-			else $(".inputAlert").attr('id', 'showAlert'); 
-
+	$("button.remove").on("click", function (event) {
+		removeCommentFromInputBox();
+	});
+	
+	// Trigger 
+	$(".comment-input input").on("keypress", function (event) {
+		if (event.keyCode === 13) {
+			addCommentFromInputBox();
 		}
 	});
 };
 
 $(document).ready(main);
+
+// var main = function () {
+// 	"use strict";
+	
+// 	$(".comment-input button").on("click", function (event) {
+// 		var $new_comment = $("<p>");
+// 		var $input;
+
+// 		$input = $(".comment-input input").val();
+// 		if ($input != ""){
+// 			$new_comment.text($input);
+// 			$new_comment.hide();$(".comments").append($new_comment);
+// 			$(".comment-input input").val("");
+// 			$(".inputAlert").attr('id', '');
+// 		}
+// 		else $(".inputAlert").attr('id', 'showAlert'); 
+// 	});
+
+// 	$(".comment-input input").on("keypress", function(event){
+// 		// console.log("Button code " + event.keyCode + " pressed.");
+// 		var $new_comment = $("<p>");
+// 		var $input;
+		
+// 		if (event.keyCode == 13){
+// 			$input = $(".comment-input input").val();
+// 			if ($input != ""){
+// 				$new_comment.text($input);
+// 				$(".comments").append($new_comment);
+// 				$(".comment-input input").val("");
+// 				$(".inputAlert").attr('id', '');
+// 			}
+// 			else $(".inputAlert").attr('id', 'showAlert'); 
+
+// 		}
+// 	});
+// };
+
+// $(document).ready(main);
 
