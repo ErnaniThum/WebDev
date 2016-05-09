91337cb94dd690933faa969179cc6d37b412e4dd
Ernani Vinicius Thum <viniciusthm@hotmail.com>
2016-05-09 20:56:33 -0300

    added fadeout FIFO for remove button - ex 4

--- Chapter4/example2/app.js
+++ Chapter4/example2/app.js
@@ -2,7 +2,7 @@
 var main = function () {
 	"use strict";
 	
-	var addCommentFromInputBox = function () {
+	function addCommentFromInputBox() {
 		var $new_comment;
 		if ($(".comment-input input").val() !== "") {
 			$new_comment = $("<p>").text($(".comment-input input").val());
@@ -10,18 +10,19 @@ var main = function () {
 			$(".comments").append($new_comment);
 			$new_comment.fadeIn();
 			$(".comment-input input").val("");
-
-			$(".inputAlert").hide();
+			console.log($new_comment);
+			$(".inputAlert").fadeOut();
 		}
 		else $(".inputAlert").fadeIn();
 	};
 
 	function removeCommentFromInputBox() {
-		
-		var lastParagraph = $(".comments p").size();
-		if (lastParagraph > 0){
-			$(".comments p")[lastParagraph-1].remove();
-		}
+	   var variable= 0;
+	   console.log($('.comments p:nth-child(1)'));
+	   
+	   $('.comments p:nth-child(1)').fadeOut("slow", function(){
+	   		$('.comments p:nth-child(1)').remove();
+	   });
 	};
 
 	// Trigger click message field
@@ -41,43 +42,5 @@ var main = function () {
 	});
 };
 
+
 $(document).ready(main);
-
-// var main = function () {
-// 	"use strict";
-	
-// 	$(".comment-input button").on("click", function (event) {
-// 		var $new_comment = $("<p>");
-// 		var $input;
-
-// 		$input = $(".comment-input input").val();
-// 		if ($input != ""){
-// 			$new_comment.text($input);
-// 			$new_comment.hide();$(".comments").append($new_comment);
-// 			$(".comment-input input").val("");
-// 			$(".inputAlert").attr('id', '');
-// 		}
-// 		else $(".inputAlert").attr('id', 'showAlert'); 
-// 	});
-
-// 	$(".comment-input input").on("keypress", function(event){
-// 		// console.log("Button code " + event.keyCode + " pressed.");
-// 		var $new_comment = $("<p>");
-// 		var $input;
-		
-// 		if (event.keyCode == 13){
-// 			$input = $(".comment-input input").val();
-// 			if ($input != ""){
-// 				$new_comment.text($input);
-// 				$(".comments").append($new_comment);
-// 				$(".comment-input input").val("");
-// 				$(".inputAlert").attr('id', '');
-// 			}
-// 			else $(".inputAlert").attr('id', 'showAlert'); 
-
-// 		}
-// 	});
-// };
-
-// $(document).ready(main);
-
