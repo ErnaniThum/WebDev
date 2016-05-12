88f8e3c888a8ed0d7c25dce6a7cc8790e12dbea6
Ernani Vinicius Thum <viniciusthm@hotmail.com>
2016-05-11 23:23:10 -0300

    amazeriffic javascript optimization

--- Chapter4/Amazeriffic/script.js
+++ Chapter4/Amazeriffic/script.js
@@ -1,23 +1,29 @@
+// var main = function () {
+// 	"use strict";
+	
+// 	$(".tabs li:nth-child(1) a").on("click", changePage);
+// 	$(".tabs li:nth-child(2) a").on("click", changePage);
+// 	$(".tabs li:nth-child(3) a").on("click", changePage); 
+	
+// 	function changePage(){
+
+// 		$(".tabs li a").removeClass("active");
+// 		$(this).addClass("active");
+// 		console.log(this);
+// 	}
+
+// };
+
 var main = function () {
 	"use strict";
-	
-	$(".tabs li:nth-child(1) a").on("click", function () {
-		$(".tabs li a").removeClass("active");
-		$(".tabs li:nth-child(1) a").addClass("active");
-		//$("main .content").empty();
-		return false;
-	});
-	$(".tabs li:nth-child(2) a").on("click", function () {
-		$(".tabs li a ").removeClass("active");
-		$(".tabs li:nth-child(2) a").addClass("active");
-		//$("main .content").empty();
-		return false;
-	});
-	$(".tabs li:nth-child(3) a").on("click", function () {
-		$(".tabs li a").removeClass("active");
-		$(".tabs li:nth-child(3) a").addClass("active");
-		//$("main .content").empty();
-		return false;
+	$(".tabs li a").toArray().forEach(function (element) {
+		// create a click handler for this element
+		$(element).on("click", function () {
+			$(".tabs li a").removeClass("active");
+			$(this).addClass("active");
+			$("main .content").empty();
+			return false;
+		});
 	});
 };
 
